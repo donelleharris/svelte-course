f84cf8cc1820a534af9b545a6521a516240149dd
Donelle Harris <donelle.harris@gmail.com>
2021-02-27 22:42:59 -0600

    Create initial files for Udemy Svelte.js - The Complete Guide
    (incl. Sapper.js)

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = "Donelle";
+	let age = 52;
+
+	function incrementAge(){
+		age += 1;
+	}
+	function decrementAge(){
+		age -= 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Increase age</button>
+<button on:click="{decrementAge}">Decrease age</button>
